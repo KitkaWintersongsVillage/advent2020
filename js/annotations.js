adff50b92e787cb9cd2aa86d365b3a18668f819b
KitkaWintersongsVillage <75272590+KitkaWintersongsVillage@users.noreply.github.com>
2020-12-01 16:09:51 -0800

    Update annotations.js

--- js/annotations.js
+++ js/annotations.js
@@ -5,50 +5,45 @@ const baseURL = 'http://www.kitka.org/wintersongsdaily/dec'
 const storage = window.localStorage;
 
 const stars = JSON.parse(storage.getItem('starsInStorageArray') || "[]");
-// const stars = [];
-console.log(stars);
-
 
 function createBorder(boxId, x, y, w, h, pulsing, gone){
     const anchor = document.createElement('a');
     anchor.className= 'divLink';
     anchor.href = `${baseURL}${boxId}`;
     const div = document.createElement('div');
-    // div.appendChild(anchor);
     div.style.left = `${x}px`;
     div.style.top = `${y}px`;
     div.style.width = `${w}px`;
     div.style.height = `${h}px`;
     div.className = 'box';
     div.id = `box${boxId}`;
-    if(gone){
-        div.className += '';
-    }
+
     if(pulsing){
         div.className += ' pulsing';
-        // div.style.animation = 'pulse 2s infinite';
         div.style.animation = 'pulse 2s infinite';
-        div.addEventListener('click', (e)=>{
-            // console.log(id);
-            // console.log(starOffsets[id-1]);
-            const {id, x, y} = starOffsets[boxId - 1];
-            const rotVal = Math.random() * 180;
-            const found = stars.find(el => el.id === boxId);
-            if(!found){
-                const newStar = {id, x, y, rotVal};
-                stars.push(newStar);
-                storage.setItem('starsInStorageArray', JSON.stringify(stars));
-                createNewStar(newStar);
-                // divs[boxId - 1].className = 'box';
-                // divs[star.id-1].className = 'box';
-               
-            } 
-            
-            setTimeout(() => changePage(boxId), 500);
-            // e.target.className = '';
-            // e.target.className = 'box';
-        })
     }
+    if(gone){
+        div.className = 'box no-border';
+        div.style.animation = '';
+    }
+
+    div.addEventListener('click', (e) => {
+        const {id, x, y} = starOffsets[boxId - 1];
+        const rotVal = Math.random() * 180;
+        const found = stars.find(el => el.id === boxId);
+        setTimeout(() => changePage(boxId), 500);
+        if(!found){
+            const newStar = {id, x, y, rotVal};
+            stars.push(newStar);
+            storage.setItem('starsInStorageArray', JSON.stringify(stars));
+            createNewStar(newStar);
+            boxes[boxId - 1].gone = true;
+            boxes[boxId - 1].pulsing = false;
+        } 
+        boxes[boxId - 1].gone = true;
+        boxes[boxId - 1].pulsing = false;
+        e.target.className = 'box no-border';
+    })
     myContainer.appendChild(div);
     return div
 }
@@ -67,11 +62,7 @@ function displayStar(id, x, y, rotVal, growing){
     star.className = 'star';
     star.id = `star${id}`;
     const img = document.createElement('img');
-    // const starImg = starImages[Math.floor(Math.random() * starImages.length)];
-    // const starImg = starImages[0];
-    // const starImg = 'img/stars/pink-star.png';
     img.src = 'img/stars/use-star.png';
-   // img.src = starImg;
     star.appendChild(img);
     myContainer.appendChild(star);
 }
@@ -81,13 +72,15 @@ function loadStars(stars){
         displayStar(star.id, star.x, star.y, star.rotVal, false);
         // divs[star.id].className = '';
         // console.log(divs);
-        divs[star.id - 1 ].className = 'hide';
+        divs[star.id - 1 ].className = 'box no-border';
+        divs[star.id - 1 ].style.animation = '';
+       
     })
 }
 
 function createNewStar(star){
     displayStar(star.id, star.x, star.y, star.rotVal, true);
-    divs[star.id - 1 ].className = 'hide';
+    divs[star.id - 1 ].className = 'box no-border';
 }
 
 divs.forEach((div, i) => {
@@ -100,5 +93,5 @@ loadStars(stars);
 
 function changePage(i){
     document.location.href = `${baseURL}${i}`;
-    // loadStars(stars);
+    console.log(i);
 }
